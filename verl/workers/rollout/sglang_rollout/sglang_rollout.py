1f9bce7de8db2754775f83a047facc08c0807b19
langfeng <langfeng.cs@gmail.com>
2025-10-20 17:13:56 +0800

    fix sglang bug

--- verl/workers/rollout/sglang_rollout/sglang_rollout.py
+++ verl/workers/rollout/sglang_rollout/sglang_rollout.py
@@ -283,6 +283,11 @@ class SGLangRollout(BaseRollout):
         self.sharding_manager = None
         # offload
         if self._tp_rank == 0:
+            try:
+                loop = asyncio.get_event_loop()
+            except RuntimeError:
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
             self._engine.release_memory_occupation()
         self.is_sleep = True
         
