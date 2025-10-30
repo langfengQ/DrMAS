f873611faa2e916972dc77c0ea65e6a19070cd39
Lang Feng <94028949+langfengQ@users.noreply.github.com>
2025-10-30 21:20:43 +0800

    IMPORTANT: support multiagent for math (#48)
    * IMPORTANT: add multiagent-math
    
    * add aime24&math500 to the test dataset & adjust agent

---
+++ examples/multi_agent_trainer/run_math.sh
@@ -0,0 +1,95 @@
+set -x
+export CUDA_VISIBLE_DEVICES=4,5
+
+##################### Agent Configurations #####################
+agent_ids='["Solver Agent","Verifier Agent"]'
+model_ids='["Qwen/Qwen3-1.7B","Qwen/Qwen3-1.7B"]'
+model_sharing=False
+
+orchestra_type=math
+
+# Agent-specific parameter override (only support actor_rollout_ref)
+agent_specific_parameters='["actor.optim.lr","actor.ppo_micro_batch_size_per_gpu"]'
+actor_optim_lr='[1e-6,1e-6]'
+actor_ppo_micro_batch_size_per_gpu='[2,2]'
+
+##################### Training Configurations #################
+
+train_data_size=32
+val_data_size=80
+group_size=8
+ppo_mini_update_num=1
+
+max_prompt_length=8000
+max_response_length=4000
+
+
+###################### Algorithm Configurations #################
+
+algorithm=grpo
+group_by_agent_id=True
+
+
+####################### Other Configurations #####################
+
+model_name_tag=$(jq -r '.[]' <<< "$model_ids"  | awk -F/ '{print $NF}' | tr '[:upper:]' '[:lower:]' | tr '-' '_' | paste -sd_)
+agent_name_tag=$(jq -r '.[]' <<< "$agent_ids" | sed 's/ Agent//g' | tr '[:upper:]' '[:lower:]' | tr '-' '_' | paste -sd_)
+
+combined_tag="${agent_name_tag}_${model_name_tag}"
+
+experiment_name="${combined_tag}_share${model_sharing}_groupbyagent${group_by_agent_id}_${max_turn}turn_${max_prompt_length}prompt_${max_response_length}res"
+
+TRAIN_DATA="/home/langfeng/data/dapo_filter/train.parquet"
+VAL_DATA="/home/langfeng/data/dapo_filter/test.parquet"
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=$algorithm \
+    data.train_files=$TRAIN_DATA \
+    data.val_files=$VAL_DATA \
+    data.train_batch_size=$train_data_size \
+    data.val_batch_size=$val_data_size \
+    data.max_prompt_length=$max_prompt_length \
+    data.max_response_length=$max_response_length \
+    data.filter_overlong_prompts=True \
+    +data.apply_chat_template_kwargs.enable_thinking=False \
+    data.truncation='right' \
+    data.return_raw_chat=True \
+    actor_rollout_ref.model.path=null \
+    actor_rollout_ref.actor.optim.lr=$actor_optim_lr \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.use_adaptive_ppo_mini_batch_size=True \
+    actor_rollout_ref.actor.ppo_mini_update_num=$ppo_mini_update_num \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=$actor_ppo_micro_batch_size_per_gpu \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.name=sglang \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.enable_chunked_prefill=False \
+    actor_rollout_ref.rollout.enforce_eager=False \
+    actor_rollout_ref.rollout.free_cache_engine=False \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.use_invalid_action_penalty=True \
+    actor_rollout_ref.actor.invalid_action_penalty_coef=0.01 \
+    algorithm.group_by_agent_id=$group_by_agent_id \
+    env.env_name=math \
+    env.seed=0 \
+    env.rollout.n=$group_size \
+    agent.agent_ids="$agent_ids" \
+    agent.model_ids="$model_ids" \
+    agent.model_sharing=$model_sharing \
+    agent.agent_specific_parameters=$agent_specific_parameters \
+    agent.orchestra_type=$orchestra_type \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name='multiagent_math' \
+    trainer.experiment_name="$experiment_name" \
+    trainer.n_gpus_per_node=2 \
+    trainer.nnodes=1 \
+    trainer.save_freq=100 \
+    trainer.test_freq=50 \
+    trainer.total_epochs=10 \
+    trainer.val_before_train=True $@
