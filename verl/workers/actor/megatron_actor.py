62a4a05b56938f8e556559e55ca6cb52cee12311
langfeng <langfeng.cs@gmail.com>
2026-04-01 20:30:35 +0800

    gspo

--- verl/workers/actor/megatron_actor.py
+++ verl/workers/actor/megatron_actor.py
@@ -38,7 +38,7 @@ from omegaconf import OmegaConf
 from torch import nn
 
 from verl import DataProto
-from verl.trainer.ppo.core_algos import agg_loss, compute_policy_loss, kl_penalty
+from verl.trainer.ppo.core_algos import agg_loss, compute_policy_loss, compute_policy_loss_gspo, kl_penalty
 from verl.utils.debug import GPUMemoryLogger
 from verl.utils.debug.profile import Profiler
 from verl.utils.megatron.pipeline_parallel import make_batch_generator
@@ -343,7 +343,13 @@ class MegatronPPOActor(BasePPOActor):
                 clip_ratio_low = self.config.clip_ratio_low if self.config.clip_ratio_low is not None else clip_ratio
                 clip_ratio_high = self.config.clip_ratio_high if self.config.clip_ratio_high is not None else clip_ratio
                 clip_ratio_c = meta_info["clip_ratio_c"]
-                pg_loss, pg_clipfrac, ppo_kl, pg_clipfrac_lower = compute_policy_loss(
+                loss_mode = self.config.get("policy_loss", {}).get("loss_mode", "vanilla")
+                if loss_mode == "gspo":
+                    policy_loss_fn = compute_policy_loss_gspo
+                else:
+                    policy_loss_fn = compute_policy_loss
+
+                pg_loss, pg_clipfrac, ppo_kl, pg_clipfrac_lower = policy_loss_fn(
                     old_log_prob=old_log_prob,
                     log_prob=log_prob,
                     advantages=advantages,
